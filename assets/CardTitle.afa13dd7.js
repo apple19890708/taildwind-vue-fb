46b0ccb46b1a8769b64af865b99dd44f9839923c
Tommy <tommychen@applyeasypro.com>
2022-10-03 22:22:26 +0800

    deploy

--- assets/CardTitle.afa13dd7.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as r,t as o}from"./index.402fd719.js";const s={class:"shadow-block relative mb-8 flex items-center justify-center border-2 border-black bg-white p-5 font-azeret text-xl"},l={props:{title:{type:String}},setup(e){return(a,c)=>(t(),r("div",s,o(e.title),1))}};export{l as _};
