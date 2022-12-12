65b9fa3cf2f6133de9a513d157ce7703d9581ca4
Tommy <tommychen@applyeasypro.com>
2022-12-12 11:10:04 +0800

    [更換部屬機器] heroku to render

--- src/global/constant.js
+++ src/global/constant.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://mysterious-cove-77258.herokuapp.com';
+export const API_URL = 'https://mysterious-cove-77258.onrender.com';
 export const TEST_API_URL = 'http://localhost:3005';
