646622134145c5d882c2ea0b7d7384333ee3fdf0
Tommy <tommychen@applyeasypro.com>
2023-04-01 15:28:01 +0800

    deploy

--- assets/ChatRoomView.146c3973.js
+++
@@ -1,1 +0,0 @@
-import{V as e,W as t,j as a,u as n,ad as c,o as m}from"./index.709cefc9.js";const l={setup(u){const r=e(),{room:s}=t(r);return(f,p)=>{var o;return m(),a(c,{roomInfo:(o=n(s))==null?void 0:o[0]},null,8,["roomInfo"])}}};export{l as default};
